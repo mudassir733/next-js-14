d7b7f991f548a381832f0e4992eff3380100aae0
Mudassir Ali <deadlool45@gmail.com>
2024-09-23 11:02:39 +0500

    redirects in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/user',
+                permanent: true,
+            },
+            {
+                source: '/',
+                destination: '/user',
+                permanent: false,
+            }
+        ]
+    }
+};
 
 export default nextConfig;
